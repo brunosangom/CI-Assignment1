6aee4763bc56c8429165eb254a1cea35fe505745
SheenaGit <91433803+SheenaGit@users.noreply.github.com>
2024-10-25 12:21:06 +0200

    Added synthetic data generation and plotting

--- iris_stats.r
+++ iris_stats.r
@@ -8,3 +8,12 @@ print(
     group_by(Species) %>%
     summarise(across(where(is.numeric), mean))    
 )
+
+cov_matrices <- data %>%
+    group_by(Species) %>%
+    summarise(cov_matrix = list(cov(select(cur_data(), where(is.numeric))))) %>%
+    pull(cov_matrix)
+
+# `cov_matrices` will contain the covariance matrix for each Species as a list of matrices
+names(cov_matrices) <- unique(data$Species)
+print(cov_matrices)
